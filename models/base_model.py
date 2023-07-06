27e07287ef2324ad999ea62066dca5f4b21fc64d
Luis <luisayala323@gmail.com>
2023-07-06 18:09:36 -0400

    methods documentation

--- models/base_model.py
+++ models/base_model.py
@@ -15,17 +15,34 @@ class BaseModel:
     """
 
     def __init__(self):
-        self.id = str(uuid.uuid4())
+        """
+        Initialize instance attributes
+            Attrs:
+                id (str): instance identity
+                created_at: date of instance creation
+                updated_at: date of instance attrs change
+        """
+        self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
 
     def __str__(self):
+        """
+        Returns string representation of instance
+        """
         return f"[{self.__class__.__name__}] ({self.id}){self.__dict__}"
-    
+
     def save(self):
+        """
+        Actualize time of instance attributes(time & date)
+        """
         self.updated_at = datetime.now()
-        
+
     def to_dict(self):
+        """
+        Returns a dictionary containing all keys/values
+        of __dict__ of the instance including new attrs
+        """
         obj_dict = {}
         obj_dict['__class__'] = self.__class__.__name__
         self.__dict__['created_at'] = (datetime.isoformat(
